d45a06557bc319a00d78853923bb8add290982e8
Tanatcha <tnatcha1304@gmail.com>
2023-11-11 10:47:52 +0700

    change sum array functionality

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 function sum(a,b){
-    return a+b
+    if(Array.isArray(a)){
+       return a.reduce((acc, n) => acc + n, 0)
+    }else if (typeof a === 'number' && typeof b === 'number'){
+        return a+b
+    }
+    
 }
 
 module.exports = {
